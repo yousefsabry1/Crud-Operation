b9aeb3e5a9744078232857b019dfc299693d04fd
yousefsabry1 <yousef.msr3@gmail.com>
2024-06-23 04:00:20 +0300

    Second commit ( week 10 , session 1 )

--- js/index.js
+++ js/index.js
@@ -5,10 +5,19 @@ var price = document.getElementById('ProductPrice');
 var des = document.getElementById('ProductDes');
 var cat = document.getElementById('ProductCat');
 var img = document.getElementById('ProductImg');
+var search = document.getElementById('ProductSearch');
 
-var productContainer=[];
+var productContainer;
 
+if(localStorage.getItem("product")==null)
+    {
+        productContainer = [];
+    }
 
+    else{
+        productContainer= JSON.parse(localStorage.getItem("product"));
+        displayData(productContainer);
+    }
 
 
 function addproduct()
@@ -18,11 +27,16 @@ function addproduct()
         price:price.value,
         cat:des.value,
         des:cat.value,
-        img:img.value
+        img:`images/${ProductImg.files[0].name}`
     };
 
     productContainer.push(product);
-    displayData()
+
+    localStorage.setItem("product" , JSON.stringify(productContainer));
+
+
+
+    displayData(productContainer);
     clearform();
 };
 
@@ -35,25 +49,50 @@ function clearform()
     img.value=null;
 }
 
-function displayData()
+function displayData(arr)
 {
 
-    console.log(productContainer[0]);
+    
 
     var cont = '';
-    for(var i = 0 ; i < productContainer.length ; i++)
+    for(var i = 0 ; i < arr.length ; i++)
         {
             cont += `<div class="col-md-3">
             <div class="card">
-                <img src="images/WhatsApp Image 2023-04-07 at 14.32.29.jpg" class="card-img-top" alt="...">
+                <img src="${arr[i].img}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="fs-5">Product Name : <span class="fs-6">${productContainer[i].code}</span></h5>
-                  <h5 class="fs-5">Product Price : <span class="fs-6">${productContainer[i].price}</span></h5>
-                  <h5 class="fs-5">Product Category : <span class="fs-6">${productContainer[i].cat}</span></h5>
-                  <h5 class="fs-5">Product Description : <span class="fs-6">${productContainer[i].des}</span></h5>
+                  <h5 class="fs-5">Product Name : <span class="fs-6">${arr[i].code}</span></h5>
+                  <h5 class="fs-5">Product Price : <span class="fs-6">${arr[i].price}</span></h5>
+                  <h5 class="fs-5">Product Category : <span class="fs-6">${arr[i].cat}</span></h5>
+                  <h5 class="fs-5">Product Description : <span class="fs-6">${arr[i].des}</span></h5>
+                  <button onclick="deleteproduct(${i})" class="delbutton">Delete</button>
+                  <button class="upbutton">Update</button>
                 </div>
             </div>
         </div>`
-        }
+        };
         document.getElementById('demo').innerHTML=cont;
+}
+
+function deleteproduct(index)
+{
+    
+    productContainer.splice(index,1);
+    displayData(productContainer);
+    localStorage.setItem("product" , JSON.stringify(productContainer));
+}
+
+function searchproduct()
+{
+    x = search.value;
+    var searchContainer = []
+    for( var i = 0 ; i <productContainer.length ; i++)
+        {
+            if(productContainer[i].code.toLowerCase().includes(x.toLowerCase())==true)
+                {
+                    searchContainer.push(productContainer[i]);
+                    displayData(searchContainer);
+                }
+        }
+        
 }
